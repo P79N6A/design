228dca881ece22a916393828bc41658ca4933744
yanzuo.zp <yanzuo.zp@alibaba-inc.com>
2018-11-29 15:15:19 +0800

    fixed all

---
+++ build/manage/locale.js
@@ -0,0 +1,22 @@
+var messages = {
+  体验设计指南: 'Experience design guide',
+  一套符合B类场景的设计体系: 'A design system that conforms to B type scenario',
+  查看更多: 'To view more',
+  品牌概览: 'An overview of the brand',
+  '通过品牌了解我们的愿景及服务对象，帮助你在设计的时候更好的进行判断和选择，并以同样的性格特质向用户呈现。':
+    'Understanding our vision and service target through the brand helps you better judge and choose when designing and present to users with the same personality traits.',
+  品牌调性: 'Brand essence',
+  '在这里了解我们以何种语调向用户传递我们的声音。':
+    'Find out what tone of voice we deliver to our users.',
+  视觉指南: 'Visual guide',
+  '在这里了解我们可以使用的设计元素，这些元素符合整个品牌的定义，并且经过设计实验的验证，可以安心的被使用。':
+    'Here, we learn about the design elements that we can use. These elements conform to the definition of the whole brand and can be used safely after being verified by design experiments.',
+  多媒体指南: 'Multimedia guide',
+  '不同载体及形式的设计指南，包括VR、3D。':
+    'Design guidelines for different carriers and forms, including VR and 3D.',
+  设计资料库: 'Design database',
+  '在这里，可以看到历史积累的各项用户体验数据及资料，并下载辅助设计的工具。':
+    'Here, you can see all kinds of user experience data and materials accumulated in history, and download the tools of auxiliary design.',
+  '本网站由阿里巴巴国际用户体验部 - B2B品牌组维护 我们致力于通过提升体验来帮助全球中小买卖家。 如果您有任何建议，请随时与我们联系。':
+    'This website is maintained by the alibaba international user experience department-b2b brand group and we are committed to helping small and medium-sized buyers and sellers around the world by enhancing the experience. If you have any Suggestions, please feel free to contact us.',
+}
