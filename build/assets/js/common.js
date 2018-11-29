852a3341c0ddb522797d75a9cee8dc180bb125bb
yanzuo.zp <yanzuo.zp@alibaba-inc.com>
2018-11-29 15:15:19 +0800

    fixed all

--- build/assets/js/common.js
+++ build/assets/js/common.js
@@ -1,1 +1,205 @@
-"use strict";$(function(){new Vue({el:".layout-header",data:function(){return{menuFlex:!0}},mounted:function(){$(document).on("click",".layout-header .menu .icon-menu",function(e){var n=$(".layout-header .nav");n.addClass("active"),$(document).on("click.closeNav",function(e){$.contains(n.get(0),e.target)||(n.removeClass("active"),$(document).off("click.closeNav"))})}),$(document).on("click",".layout-header .menu .icon-search",function(e){var n=$(".layout-header .menu"),o=$(".layout-header .nav"),c=$(".layout-header .menu-search");o.toggleClass("flex"),c.toggleClass("flex"),c.focus(),$(document).on("click.closeFlex",function(e){$.contains(n.get(0),e.target)||(o.toggleClass("flex"),c.toggleClass("flex"),$(document).off("click.closeFlex"))})})}})});
+'use strict'
+var commonMessage = {
+  '原 fusion 官网已迁移:': 'The original fusion website has been moved to:',
+  点击前往: 'Click Here',
+  品牌: 'BRAND',
+  设计指导: 'GUIDELINES',
+  资料库: 'LIBRARY',
+  '本网站由阿里巴巴国际用户体验部 - B2B品牌组维护':
+    'This website is maintained by alibaba international user experience department-b2b brand group',
+  '我们致力于通过提升体验来帮助全球中小买卖家。 如果您有任何建议，请随时与我们联系。':
+    'We are committed to helping small and medium-sized traders around the world by enhancing the experience. If you have any Suggestions, please feel free to contact us.',
+  品牌故事: 'Brand Story',
+  愿景: 'Vision',
+  目标用户: 'Target Audience',
+  品牌个性: 'Brand Personality',
+  关键词: 'Keywords',
+  设计原则: 'Design Principles',
+  品牌调性: 'Brand Voice',
+  产品: 'Product',
+  邮件: 'EDM',
+  文章: 'Article',
+  设计语言: 'Design Language',
+  设计语言概述: 'Overview',
+  元素概述: 'Design Element',
+  设计关系: 'Relationship',
+  设计框架: 'Design Framework',
+  什么是设计框架: 'What is it',
+  怎样运用设计框架: 'How to use it',
+  视觉规范: 'Visual Guidelines',
+  色彩: 'Color',
+  栅格: 'Grid',
+  间距: 'Spacing',
+  图标: 'Icon',
+  插画: 'Illustrations',
+  字体排版: 'Typography',
+  图片: 'Photograph',
+  创意设计: 'Creative Design',
+  语境: 'Context',
+  范围: 'Range',
+  '方法 & 过程': 'Method & Process',
+  '管理 & 用户感知验证': 'Management & User verification',
+  体验数据: 'Experience Data',
+  词汇库: 'Glossary',
+  工具: 'Tools',
+  '了解更多': 'Learn More',
+  '如有任何疑问，': 'If you have any question, ',
+  '可联系yanzuo.zp@alibaba-inc.com': 'please email: yanzuo.zp@alibaba-inc.com',
+  '即将上线，敬请期待': 'Coming Soon...',
+  '设计元素': 'Element',
+  '设计模式库': 'Design Library',
+  '词汇库': 'Glossary',
+  '体验数据库': 'Experience Database',
+  '设计思维': 'Design Thinking',
+  'Token规范': 'Token',
+  '点击这里': 'Click Here',
+}
+var _extends =
+  Object.assign ||
+  function(target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i]
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key]
+        }
+      }
+    }
+    return target
+  }
+var messages = _extends({}, commonMessage, messages || {})
+var init = Vue.prototype._init
+var DEFAULT_LANG = 'cn'
+var LOCALE_KEY = 'localeLanguage'
+var locales = {
+  zh: 'zh',
+  en: 'en',
+}
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true,
+    })
+  } else {
+    obj[key] = value
+  }
+  return obj
+}
+
+var realMessage = {
+  cn: Object.keys(messages).reduce(function(map, item) {
+    return Object.assign({}, map, _defineProperty({}, item, item))
+  }, {}),
+  en: Object.keys(messages).reduce(function(map, item) {
+    return Object.assign({}, map, _defineProperty({}, item, messages[item]))
+  }, {}),
+}
+
+var i18n = new VueI18n({
+  locale: 'cn',
+  messages: realMessage,
+})
+
+function setup(lang) {
+  if (lang === undefined) {
+    lang = window.localStorage.getItem(LOCALE_KEY)
+    if (locales[lang] === undefined) {
+      lang = DEFAULT_LANG
+    }
+  }
+  window.localStorage.setItem(LOCALE_KEY, lang)
+
+  Object.keys(locales).forEach(lang => {
+    document.body.classList.remove(`lang-${lang}`)
+  })
+  document.body.classList.add(`lang-${lang}`)
+  document.body.setAttribute('lang', lang)
+
+  Vue.config.lang = lang
+  i18n.locale = lang
+}
+
+setup()
+
+Vue.prototype._init = function(options) {
+  init.call(
+    this,
+    _extends(
+      {
+        i18n: i18n,
+      },
+      options
+    )
+  )
+}
+
+var colorOverview
+var promotionColors
+
+$(function() {
+  new Vue({
+    el: '#app',
+    i18n: i18n,
+    data: function() {
+      return {
+        lang: i18n.lang,
+        menuFlex: !0,
+        activeName: 'tab1',
+        colorOverview: colorOverview,
+        promotionColors: promotionColors,
+        lang: window.localStorage.getItem(LOCALE_KEY),
+      }
+    },
+    mounted: function() {
+      $(document).on('click', '.layout-header .menu .icon-menu', function(e) {
+        var n = $('.layout-header .nav')
+        n.addClass('active'),
+          $(document).on('click.closeNav', function(e) {
+            $.contains(n.get(0), e.target) ||
+              (n.removeClass('active'), $(document).off('click.closeNav'))
+          })
+      }),
+        $(document).on('click', '.layout-header .menu .icon-search', function(
+          e
+        ) {
+          var n = $('.layout-header .menu'),
+            o = $('.layout-header .nav'),
+            c = $('.layout-header .menu-search')
+          o.toggleClass('flex'),
+            c.toggleClass('flex'),
+            c.focus(),
+            $(document).on('click.closeFlex', function(e) {
+              $.contains(n.get(0), e.target) ||
+                (o.toggleClass('flex'),
+                c.toggleClass('flex'),
+                $(document).off('click.closeFlex'))
+            })
+        })
+
+      docsearch({
+        apiKey: '10ccb292caf1f598900a539ee6f74f44',
+        indexName: 'fusion_design',
+        inputSelector: '.menu-search',
+        debug: true,
+      })
+
+      document.title = i18n.t(document.title)
+    },
+    methods: {
+      switchLanguage() {
+        var language = this.lang === 'cn' ? 'en' : 'cn'
+        window.localStorage.setItem(LOCALE_KEY, language)
+        window.location.reload()
+      },
+      copyColor: function(e) {
+        this.$message({ type: 'success', message: 'Copy ' + e + ' success!' }),
+          copy(e)
+      },
+      handleTabClick() {},
+    },
+  })
+})
---
+++ build/icon/locale.js
@@ -0,0 +1,50 @@
+var messages = {
+'图标是图形界面的重要组成部分，具有指导和标识性质的图形。好的图标应该是引用现实中的物品，简化图形元素，具有隐喻作用，能帮助用户理解，指示和引导用户顺利地完成操作。': 
+'Icons, as an important part of the graphical user interface, are the graphics with instruction and identification properties. Good icons should be drawn from real-life items and simplified into graphical elements, with metaphorical effect. They help users understand, instruct and guide them to complete the operation smoothly.',
+'场景、分类、目标': 'Scenarios, Types and Goals',
+'图标应用场景：受空间限制对某些含义进行快速展示': 'Icons application scenario: Quick display of some implications due to space limitations',
+'图标分类：': 'Types of icons:',
+'功能操作类：操作后有相应反馈': 'Functional operation: Provides corresponding feedbacks after operation',
+'指示符号类：用户已熟悉约定的功能标识，适合以图标展示，帮助用户快速理解识别': 'Indicative symbol: Functional identifiers that are known to the users and suitable for icon display help users understand and recognize easily',
+'用户体验指标：': 'User experience indicator:',
+'看得懂、懂得快': 'Easy and quick to understand',
+'图标应用场景：用户已熟悉约定的功能标识，适合以图标展示，帮助用户快速理解识别': 
+'Icons application scenario: Functional graphics that are known to the users and suitable for icon display help users understand and recognize easily',
+'指示符号类：指示和引导用户方向或隐藏内容': 'Indicative symbol: Instructs and guides users to directions and hiding content',
+'功特定服务类：具有独立功能服务的产品标识': 'Specific functional services: Product identifiers with separate functional services',
+'图标应用场景：对信息的辅助说明，列如利益点、状态描述、流程引导': 'Icons application scenario: Supplementary description of information, such as interest points, status description, process guidance',
+'图标应用场景：传达网站特定的功能服务': 'Icons application scenario: Displays site-specific functional services',
+'辅助描述类：对内容信息的辅助说明，提升吸引力': 'Supplementary description: Provides supplementary description for content information to draw attention',
+'状态描述类：常用于信息的提示或反馈': 'Status description: Commonly used for information tips or feedback',
+'设计规范': 'Design Guidelines',
+'面积': 'Area',
+'为了保持整体图形占比和体量感的统一，图形设计时保持整体图形视觉面积基本一致，以64x64为例，相应的图形区域供参考：': 
+'In order to keep the overall graphics ratio consistent with the sense of volume, the overall graphic visual area should be consistent in graphic designs. Taking 64x64 as an example, the corresponding graphic area is for reference:',
+'单色应用': 'Monochromatic application',
+'尺寸': 'Size',
+'图形设计尺寸保持比例为1:1，增长按照4的倍数。': 'The graphic design dimensions are maintained at a ratio of 1:1 and the growth is in multiples of 4.',
+'视角': 'Angle of view',
+'保持整体图形占比的体量感与图形视角的统一，为了最大化显示事物本身特征，优先采用正前方视角，特殊情况可采用30度俯视。': 
+'Keep the uniformity of the overall graphics ratio and the angle of graphics. To maximize the display of the characteristics of the object itself, the front view is preferred, and 30-degree overlook can also be adopted under special circumstances.',
+'缺口': 'Gap',
+'为了保持品牌特征，所有线性图形需要有断线处理，断线位置需要与图标整体结构有机结合。': 
+'In order to display the characteristics of the brand, all linear graphics need line breaks, and the position should be organically combined with the overall structure of the icon.',
+'1.一笔画起笔的位置，封闭圆形的转角处。': '1. Position of the first stroke in one-stroke drawing; the corner of the closed circle.',
+'2. 外部轮廓不宜做断线，内部圆形右下角断线，不影响整体美观，图形轮廓右下角断线。': 
+'2. It is not advised to apply line breaks on the outer contour, but on the lower right corner of the inner circle or of the contour of the graphic, which does not affect the overall appearance.',
+'3. 区分物体不同层级关系（不同部件，不同平面）。': '3. Distinguish the different layers of objects (different components, different planes).',
+'使用规则': 'Rules',
+'色彩使用': 'Rules for colors',
+'在页面中作为功能操作图标时，应以文字为主，图形为辅，图标色彩使用中立色。': 'As the functional operation icons in the page, text should be the primary part aided by graphics, while the color of the icons should be neutral.',
+'在运营类场景中作为指示图标可以采用多色设计。': 'For indicative icons, multi-color design can be adopted in business operation scenarios.',
+'线面使用': 'Rules for lines and planes',
+'在小尺寸场景下可用面形提升图形的可识别度，表示当前位置或者当前已选择可用面形。': 
+'Planes can be used to enhance the recognizability of the icon in a small size scenario, indicating the current position or selection of a plane.',
+'命名规则': 'Rules for naming',
+'该规范适用于 UED上传图标时的命名，和前端的 Class命名': 'This specification applies to the naming when UED uploads an icon, and the naming of class of the front end',
+'单词字母全部小写，用中划线连接单词': 'Letters are all lowercase, and words are connected with a dash',
+'实心和空心图标保持同名，用 -filling 来区分，比如 email(空心) 和 email-filling(实心)': 
+'Solid and hollow icons use the same name and are distinguished by "-filling", such as "email" (hollow) and "email -filling" (solid)',
+'命名顺序：[icon名称]-[内容语义(可选)]-[形状(可选)]-[方向(可选)]-[filling(可选)]': 
+'Naming order: icon name - content semantics (optional) - shape (optional) - direction (optional) - filling (optional)',
+}
